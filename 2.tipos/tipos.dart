a7d4e81698a828b140e65e3786840ab57e05936e
Carlos Garay <carlosgaraymoreno1@gmail.com>
2021-04-10 11:45:23 -0500

    ejemplos tipos de datos number

---
+++ 2.tipos/tipos.dart
@@ -0,0 +1,15 @@
+void main(){
+  int edad = -52;
+  double estatura = 1.80;
+
+  //print(edad.abs());
+
+  if (edad.isNegative){
+    print("El valor es negativo");
+    print(edad.abs());
+  }
+  else{
+    print(edad.abs());
+  }
+
+}

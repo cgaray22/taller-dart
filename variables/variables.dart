8ee8792ba49117e16584aa58c7152434f7e3a2e1
Carlos Garay <carlosgaraymoreno1@gmail.com>
2021-04-10 11:20:45 -0500

    variable acepta null ejemplos

--- variables/variables.dart
+++ variables/variables.dart
@@ -2,8 +2,9 @@ void main() {
   String nombre = "juan";
   int edad = 23;
   String telefono = "3252882888";
+  int? variableAceptaNull = null;
 
   var apellido;
 
-  print(apellido);  
+  print(variableAceptaNull);
 }
